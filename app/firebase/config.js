d7a4388de50141b8a3e36f3b6febff5810e0fdd6
Dario Colaciuri <darioguitarras@gmail.com>
2024-11-12 19:33:58 -0300

    env

--- app/firebase/config.js
+++ app/firebase/config.js
@@ -1,15 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import { getAuth, GoogleAuthProvider } from "firebase/auth"
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAG2CJmRR0Y30njUiRqb3wOmxQIh-aRxZw",
-  authDomain: "next-ecommerce-9df1d.firebaseapp.com",
-  projectId: "next-ecommerce-9df1d",
-  storageBucket: "next-ecommerce-9df1d.appspot.com",
-  messagingSenderId: "28918070826",
-  appId: "1:28918070826:web:578af3ef95e94d04decd53"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
@@ -18,7 +18,4 @@ const storage = getStorage(app);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
-export { db }
-export { storage }
-export { auth }
-export { provider }
+export { db, storage, auth, provider };
